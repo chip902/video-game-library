8d766572a84a6630e2e2c1769e6a5e0a87fff535
chip902 <68761150+chip902@users.noreply.github.com>
2023-11-09 22:25:55 -0500

    Build Search Bar

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,13 +1,17 @@
 import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
 
 const ColorModeSwitch = () => {
-    const { toggleColorMode, colorMode } = useColorMode();
-    return (
-        <HStack>
-            <Switch colorScheme={'red'} isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-            <Text>Dark Mode</Text>
-        </HStack>
-    )
+  const { toggleColorMode, colorMode } = useColorMode()
+  return (
+    <HStack>
+      <Switch
+        colorScheme={'red'}
+        isChecked={colorMode === 'dark'}
+        onChange={toggleColorMode}
+      />
+      <Text whiteSpace={'nowrap'}>Dark Mode</Text>
+    </HStack>
+  )
 }
 
 export default ColorModeSwitch
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,16 @@
 import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/Logo/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
+import SearchBar from './SearchBar'
 
 const NavBar = () => {
-    return (
-        <HStack justifyContent={'space-between'} padding={'20px'}>
-            <Image padding={'20px'} src={logo} boxSize={"100px"} />
-            <ColorModeSwitch />
-        </HStack>
-    )
+  return (
+    <HStack padding={'20px'}>
+      <Image padding={'20px'} src={logo} boxSize={'100px'} />
+      <SearchBar />
+      <ColorModeSwitch />
+    </HStack>
+  )
 }
 
 export default NavBar
