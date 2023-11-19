0edb66f6d650078920fcfb1be3dbddf625347ab3
chip902 <68761150+chip902@users.noreply.github.com>
2023-11-19 15:44:30 -0500

    Refactoring with React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.tsx'
 import theme from './theme.ts'
 import './index.css'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      staleTime: 10 * 1000,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: true,
+    },
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initalcolormode} />
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initalcolormode} />
+        <App />
+        <ReactQueryDevtools />
+      </ChakraProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 )
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
-import axios from 'axios';
+import axios from 'axios'
+
+export interface FetchResponse<T> {
+  count: number
+  results: T[]
+}
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'd139899e5269464084c6b4e087c1bd1e'
-    }
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    key: 'd139899e5269464084c6b4e087c1bd1e',
+  },
 })
