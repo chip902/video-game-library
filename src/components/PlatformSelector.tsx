79e48a80631d437a170bb0379c9bd5fde0c0345c
chip902 <68761150+chip902@users.noreply.github.com>
2023-11-19 22:45:42 -0500

    create lookup hooks

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,6 +1,7 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 import usePlatforms, { Platform } from '../hooks/usePlatforms'
+import usePlatform from '../hooks/usePlatform'
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void
@@ -9,14 +10,12 @@ interface Props {
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatforms()
-  const selectedPlatform = data?.results.find(
-    (p) => p.id === selectedPlatformId
-  )
+  const platform = usePlatform(selectedPlatformId)
   if (error) return null
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || 'Platforms'}
+        {platform?.name || 'Platforms'}
       </MenuButton>
       <MenuList>
         {data?.results.map((platform) => (
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import APIClient from '../services/api-client'
+import platforms from '../data/platforms'
 
 export interface Platform {
   id: number
@@ -12,6 +13,7 @@ const usePlatforms = () =>
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000 * 180,
+    initialData: platforms,
   })
 
 export default usePlatforms
