37ed1b020295a93cf42dd49297d2f2fdaf2401ec
chip902 <68761150+chip902@users.noreply.github.com>
2023-11-19 21:16:37 -0500

    updating the use Genres hook to latest syntax

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
-import { GameQuery } from '../App'
 import { useInfiniteQuery } from '@tanstack/react-query'
+import { GameQuery } from '../App'
 import APIClient, { FetchResponse } from '../services/api-client'
 import { Platform } from './usePlatforms'
 
