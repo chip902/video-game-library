39550e1b7186d37be1255d75ff6772ef1dc750b6
chip902 <68761150+chip902@users.noreply.github.com>
2023-11-26 15:12:36 -0500

    Build game attributes and make logo link home

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,17 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { Box, HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/Logo/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchBar from './SearchBar'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <HStack padding={'20px'}>
-      <Image padding={'20px'} src={logo} boxSize={'100px'} />
+      <Link to={'/'}>
+        <Box boxSize={'100px'} padding={'20px'}>
+          <Image src={logo} />
+        </Box>
+      </Link>
       <SearchBar />
       <ColorModeSwitch />
     </HStack>
