8e3fa7d9fa812ef8909423c83d724bda35ff06c8
chip902 <68761150+chip902@users.noreply.github.com>
2023-11-26 01:04:45 -0500

    fetch game detail data

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -2,15 +2,23 @@ import { Box } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 interface Props {
-    children: ReactNode;
+  children: ReactNode
 }
 
 const GameCardContainer = ({ children }: Props) => {
-    return (
-        <Box borderRadius={10} overflow={'hidden'}>
-            {children}
-        </Box>
-    )
+  return (
+    <Box
+      _hover={{
+        transform: 'scale(1.03)',
+        transitionDuration: '0.2s',
+        transitionTimingFunction: 'ease-in-out',
+      }}
+      borderRadius={10}
+      overflow={'hidden'}
+    >
+      {children}
+    </Box>
+  )
 }
 
 export default GameCardContainer
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,9 +7,11 @@ import useGameQueryStore from '../store'
 export interface Game {
   id: number
   name: string
+  slug: string
   background_image: string
   metacritic: number
   rating_top: number
+  description_raw: string
   parent_platforms: { platform: Platform }[]
 }
 const apiClient = new APIClient<Game>('/games')
